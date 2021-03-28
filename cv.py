3559e467f5721d9f62fc523cfcbe82571a1b1299
Dishant Deshmukh <68292273+dishant2809@users.noreply.github.com>
2021-03-28 19:13:01 +0530

    Add files via upload

--- cv.py
+++ cv.py
@@ -23,31 +23,7 @@ for i in os.listdir(path):
 text = []
 for i in img_arr:
     b = pytesseract.image_to_string(i)
-    b = b.split("\n")
-    b = b.split(' ')
+    b = b.split("\n, ' ',',' ")
     text.append(b)
 
-
-
-for i in text:
-    for j in i:
-        b = re.search([0-9], j)
-
-
-
-fnl=[]
-for i in text:
-    a = re.search([0-9], i)
-    fnl.append(a)
-
-
-for i in text[0]:
-    print(i)
-
-
-no = [0,1,2,3,4,5,6,7,8,9]
-
-
-for i in text:
-   if i in no:
-       print(i)
+text
